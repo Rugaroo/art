ffcc70e26b720653fe8a85e4485d350df7415aa2
Rugaroo <63457972+Rugaroo@users.noreply.github.com>
2022-02-01 11:57:11 +0300

    7

--- script.js
+++ script.js
@@ -31,7 +31,15 @@ buttonPrev.addEventListener('click', prev);
 quizTel.addEventListener('click', quiz);
 fourBtn.addEventListener('click', last);
 
-document.body.addEventListener('touchstart', function(e){ e.preventDefault(); });
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
 
 document.addEventListener("DOMContentLoaded", function () {
   "use strict";
